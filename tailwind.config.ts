0173b8e8b9d50f8dd52ea6fe5649286070dd8669
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2026-01-05 15:51:05 +0700

    setup project and css

---
+++ tailwind.config.ts
@@ -0,0 +1,47 @@
+import plugin from "tailwindcss/plugin";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  corePlugins: {
+    container: false,
+  },
+  theme: {
+    extend: {
+      colors: {
+        orange: "#ee4d2d",
+        ev: "#1CC29F",
+      },
+      keyframes: {
+        electricity: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "100%": { backgroundPosition: "200% 50%" },
+        },
+        gradientFlow: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      animation: {
+        electricity: "electricity 3s linear infinite",
+        gradientFlow: "gradientFlow 20s ease infinite",
+      },
+    },
+  },
+  // config tự quy định css cho một cái class luôn
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        ".container": {
+          maxWidth: theme("columns.7xl"),
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: theme("spacing.4"),
+          paddingRight: theme("spacing.4"),
+        },
+      });
+    }),
+    // require('@tailwindcss/line-clamp')
+  ],
+};
